03756fdded0b8233c4917d57baae8acb4c6b09cf
vtr0 <icwt@rocketmail.com>
2025-11-22 10:49:46 +0700

    change to 4 DBs

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var _DEFAULT_CFG = {xState: { audio:{speed:1.4,volume:1,muted:false},history:{currHis:0,hisList:[],books:{}} }, 
 cDBFilesEdt: {checkEdtDelay:21_600, edt:[101,101,101,101]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
-xDBFilesCfg: {len:3,toLoad:null}};
+xDBFilesCfg: {len:4,toLoad:null}};
 var ssData = ssData || {
 	wildCartStr: "<*~~*>",
 	xBookGrp: [{grpWay:"Nhóm theo Bộ truyện",grp:[]},{grpWay:"Sắp xếp theo Người đọc/Tác giả",grp:[]},{grpWay:"Sắp xếp theo Tên truyện",grp:[]}]
